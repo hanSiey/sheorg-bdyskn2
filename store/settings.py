de89d9b3c4bec5bfb820432a81629cb6a32dde16
Hantsi <hantsitaumang@gmail.com>
2022-02-17 15:58:48 +0200

    debug

--- store/settings.py
+++ store/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-#SECRET_KEY = 'django-insecure-@q6py^bmsue^m2volhik83g301ne$z9_r34knpctuu!q_zkcmu'
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
